6dd851d131223da926c13116aece44a5ac58a153
kwang66 <kwang66@illinois.edu>
2018-11-27 21:17:12 -0600

    m

--- app/src/main/java/model/ChoreManager.java
+++ app/src/main/java/model/ChoreManager.java
@@ -11,11 +11,25 @@ public class ChoreManager {
 
     private ChoreManager() {
 
+        Chore addChore = new Chore("Take out trash","11/27/2018","05:05PM",
+                UserManager.getInstance().users.get(1),1);
+        groupChore.add(addChore);
+        addChore = new Chore("Take out trash","11/28/2018","01:05PM",
+                UserManager.getInstance().users.get(2),1);
+        groupChore.add(addChore);
+        addChore = new Chore("Buy Coca-Cola","11/30/2018","04:20PM",
+                UserManager.getInstance().users.get(3),1);
+        groupChore.add(addChore);
+        addChore = new Chore("Wash Dishes","11/31/2018","07:20AM",
+                UserManager.getInstance().users.get(2),1);
+        groupChore.add(addChore);
+
     }
 
     public static ChoreManager getInstance() {
         if(instance == null) {
             instance = new ChoreManager();
+
 
         }
         return instance;
